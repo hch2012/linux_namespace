47a9e0564ccf3b71a8cbe8c48c04869f39c8bf50
huchenghao <hch@stlc.cn>
2017-02-22 10:43:28 +0800

    modify

--- uts.c
+++ uts.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sched.h>
+#include <stdlib.h>
 
 #define STACK_SIZE (1024*1024)
 
 
 int child_main(void* args){
 	printf("子进程开始\n");
-	execv("/bin/bash",NULL);
+	execv("/bin/bash",{"/bin/bash",NULL});
 	return 1;
 }
 
@@ -17,7 +19,7 @@ int main(int argc, char const *argv[])
 {
 	void* stack;
 	stack=malloc(STACK_SIZE);
-	printf("主进程开始\n", );
+	printf("主进程开始\n");
 	int pid=clone(child_main,stack+STACK_SIZE,CLONE_NEWUTS | SIGCHLD,NULL);
 	waitpid(pid,NULL,0);
 	return 0;
